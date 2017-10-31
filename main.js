ed608ab32750414a00a882aaf80d7587d0a7376a
Camden Ko <camdenko@gmail.com>
2017-10-31 17:09:41 -0500

    set interval with refresh rate

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ const {
   getData,
 } = require('./redditBot')
 const {
-
+  accessYoutube,
 } = require('./youtubeAccess')
+const {
+  minToMs,
+} = require('./util')
+
+const refreshRate = minToMs(15)
