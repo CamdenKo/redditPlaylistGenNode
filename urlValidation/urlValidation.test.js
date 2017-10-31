282b8fd4d31270302846409eead400d10acef0ba
Camden Ko <camdenko@gmail.com>
2017-10-31 16:27:52 -0500

    jest added to global

--- urlValidation/urlValidation.test.js
+++ urlValidation/urlValidation.test.js
@@ -3,10 +3,18 @@ const {
   isYoutube,
 } = require('./urlValidation')
 
+
 describe('urlValidation', () => {
   describe('andFunc', () => {
     test('returns a function', () => {
       expect(typeof andFunc(() => true)).toBe('function')
+    })
+    test('will call each function once', () => {
+      const func1 = () => true
+      const func2 = () => true
+      andFunc(func1,func2)(null)
+      expect(func1).toHaveBeenCalledTimes(1)
+      expect(func2).toHaveBeenCalledTimes(1)
     })
   })
 })
