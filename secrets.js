6eebbf35f3826582e256d695b418c00153f9f05f
Camden Ko <camdenko@gmail.com>
2017-11-06 12:15:38 -0600

    now uploads secrets and clientid

---
+++ secrets.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reddit: {
+    username: 'CamdenK',
+    password: 'asdfjkl;',
+    clientId: 'o_mRNV4NMSwomw',
+    clientSecret: 'i6uNFTdfANH8OdkYgzfH4FC_pmQ',
+    userAgent: 'youtubePlaylistBot',
+  },
+  oauth: {
+
+  },
+}
